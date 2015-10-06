38a360173f2cea3ce08f3a6b04351cfe0e52596c
Kamil Seweryn <k.seweryn@samsung.com>
2015-10-06 12:53:23 +0200

    combining activity layout

--- app/src/main/java/pl/srw/example/databinding/MainActivity.java
+++ app/src/main/java/pl/srw/example/databinding/MainActivity.java
@@ -2,13 +2,10 @@ package pl.srw.example.databinding;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-import pl.srw.example.databinding.databinding.ContentMainBinding;
+import pl.srw.example.databinding.databinding.ActivityMainBinding;
 import pl.srw.example.databinding.model.User;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,13 +13,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ContentMainBinding binding = DataBindingUtil.setContentView(this, R.layout.content_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         User user = new User("Test", "User");
         binding.setUser(user);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
 
